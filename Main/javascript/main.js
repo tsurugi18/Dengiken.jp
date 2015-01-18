9fc7ef7745798af85df837cc8daa5d3b941f3faa
Owner <so.murata.0118@gmail.com>
2015-01-18 18:56:00 +0900

    New page
    add new page
    and fix color problem

--- Main/javascript/main.js
+++ Main/javascript/main.js
@@ -1,9 +1,13 @@
 $(function(){
-	var nav = $('#nav');
+	var nav = $('#nav'),a = $('a');
 	$(window).scroll(function(){
 		if($(this).scrollTop() > 100){
-			nav.stop().animate({opacity : '0.5'},'fast');
+			nav.stop().animate({backgroundColor : '#fff'},50);
+			a.stop().animate({color :'#000'},50);
 			console.log(1);
+		}else if($(this).scrollTop() < 99){
+			nav.stop().animate({backgroundColor : '#72dbe9'},50);
+			a.stop().animate({color :' #fff'},50);
 		}
 	});
 });
